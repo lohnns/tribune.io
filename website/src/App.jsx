c097a15dfbde64ae550bcd60e35ae3159821445a
lohnns <lohnns.dev@gmail.com>
2018-06-20 20:07:32 -0400

    create AboutMe scene

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
+import AboutMe from "./scenes/aboutme";
+
 const App = () => (
   <div>
     <h1>My tribune</h1>
+    <AboutMe />
   </div>
 );
 
---
+++ website/src/scenes/aboutme/AboutMe.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class AboutMe extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About me</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+          euismod felis quis leo elementum mattis. Curabitur ut imperdiet nibh.
+          Vestibulum bibendum ipsum et nibh consectetur, eu auctor ex imperdiet.
+          Proin ligula tellus, bibendum at nisi ac, sodales pharetra lacus. Nunc
+          vehicula est non tellus luctus sollicitudin. Morbi consequat enim eget
+          sollicitudin cursus. Aenean sed eros rutrum, tempor risus in, ultrices
+          quam. Nunc venenatis diam eu ultricies ultrices.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default AboutMe;
