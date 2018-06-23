f296f1d82621c9b8553479a679a8df7cedb3ce5b
lohnns <lohnns.dev@gmail.com>
2018-06-22 20:24:09 -0400

    configure test environment

--- website/src/setupTests.js
+++ website/src/setupTests.js
@@ -1,4 +1,16 @@
 import chai from "chai";
+import Adapter from "enzyme-adapter-react-16";
+import Enzyme from "enzyme";
+import createChaiEnzyme from "chai-enzyme";
+import dirtyChai from "dirty-chai";
+import createChaiJestDiff from "chai-jest-diff";
 
 global.jestExpect = global.expect;
 global.expect = chai.expect;
+
+chai
+  .use(dirtyChai)
+  .use(createChaiJestDiff())
+  .use(createChaiEnzyme());
+
+Enzyme.configure({ adapter: new Adapter() });
